e62b33835b355025d631f5714291e9d7ba8bc96c
Sirius <sirius@x250>
2020-08-04 15:37:08 +0800

    Flatten directory structure

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,9 +1,7 @@
-use std::net;
-
 use anyhow::Result;
 use clap::Clap;
 
-use vpn::*;
+use simple_vpn::*;
 
 fn main() {
     let args = Args::parse();
@@ -13,16 +11,14 @@ fn main() {
 #[derive(Clap)]
 #[clap(version = "0.1")]
 struct Args {
-    #[clap(short, long, default_value = "192.168.200.1")]
+    #[clap(short, long, default_value = "192.168.200.2")]
     ip: String,
-    #[clap(short, long, default_value = "0.0.0.0:3000")]
-    listen: String,
+    #[clap(short, long, default_value = "127.0.0.1:3000")]
+    server: String,
 }
 
 async fn try_run(args: Args) -> Result<()> {
-    let listener = net::TcpListener::bind(&args.listen)?;
-
-    let connector = tcp::ListenConnector { listener };
+    let connector = tcp::StreamConnector { addr: args.server };
 
     let endpoint = Endpoint::new(&args.ip, connector).await?;
 
